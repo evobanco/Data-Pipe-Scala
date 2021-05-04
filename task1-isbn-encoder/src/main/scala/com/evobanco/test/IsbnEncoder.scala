a0d2bc2a1cee322e607e1ce7e4a036281f1ed781
javi211 <martinj4991@gmail.com>
2021-05-04 23:24:12 +0200

    Desarrollo del metodo explodeIsbn() y del metodo gesTests()

--- task1-isbn-encoder/src/main/scala/com/evobanco/test/IsbnEncoder.scala
+++ task1-isbn-encoder/src/main/scala/com/evobanco/test/IsbnEncoder.scala
@@ -1,4 +1,4 @@
-package com.evobanco.test.test
+package com.evobanco.test
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
@@ -15,6 +15,35 @@ class IsbnEncoderImplicit(df: DataFrame) extends Serializable {
     * @return a data frame with new rows for each element of the ISBN code
     */
   def explodeIsbn(): DataFrame = {
-    df
+
+    /**
+     * Step 1: Filtrar las filas con ISBNs bien formados
+     */
+
+    val regexISBN = "^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{3}-[0-9]{10}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$"
+
+    val df_isbn = df.filter(col("isbn").rlike(regexISBN))
+
+    /**
+     * Step 2: Transformacion de los datos: Explode del isbn
+     */
+
+    val df_isbn_explode =
+      df_isbn.select(
+        col("name"),
+        col("year"),
+        explode(array(
+          concat(lit("ISBN-EAN: "),substring(col("isbn"),7,3)),
+          concat(lit("ISBN-GROUP: "),substring(col("isbn"),11,2)),
+          concat(lit("ISBN-PUBLISHER: "),substring(col("isbn"),13,4)),
+          concat(lit("ISBN-TITLE: "),substring(col("isbn"),17,3))
+        )).as("isbn"))
+
+    /**
+     * Step 3: Union con el df de entrada para no perder isbn malformados
+     */
+
+    df_isbn_explode.union(df)
+
   }
 }
--- task2-advanced-pivot/src/main/scala/com/evobanco/test/Pivot.scala
+++ task2-advanced-pivot/src/main/scala/com/evobanco/test/Pivot.scala
@@ -1,6 +1,8 @@
 package com.evobanco.test
 
+import com.evobanco.test.Pivot.{AggFirst, AggLast, TypeMeasurement, TypeTest}
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
 object Pivot {
@@ -24,7 +26,46 @@ class PivotImplicit(df: DataFrame) extends Serializable {
     * @return machine data pivoted
     */
   def getTests(): DataFrame = {
-    df
+
+    /**
+     * Identificar las rows del primer y ultimo test
+     */
+
+    val window = Window.partitionBy("part", "name")
+
+    val df_first_last =
+      df.withColumn("rank", row_number().over(window.orderBy("unixTimestamp")))
+        .filter(col("rank").equalTo(lit("1")))
+        .withColumn("aggregation", lit(AggFirst))
+        .drop("rank")
+        .union(df.withColumn("rank", row_number().over(window.orderBy(col("unixTimestamp").desc)))
+              .filter(col("rank").equalTo(lit("1")))
+              .withColumn("aggregation", lit(AggLast))
+              .drop("rank"))
+
+    /**
+     * Agrupar las mediciones de cada test
+     */
+
+    val df_measurement =
+      df_first_last.filter(col("mType")===TypeMeasurement)
+      .groupBy("part", "aggregation")
+      .agg(collect_list(struct("name", "value")).as("features"))
+
+    /**
+     * Unir las mediciones con los test
+     */
+
+    df_first_last.filter(col("mType")===TypeTest)
+        .join(df_measurement, Seq("part", "aggregation"))
+        .select(
+          col("part"),
+          col("location"),
+          col("name").as("test"),
+          col("testResult"),
+          col("aggregation"),
+          col("features"))
+
   }
 
 }
