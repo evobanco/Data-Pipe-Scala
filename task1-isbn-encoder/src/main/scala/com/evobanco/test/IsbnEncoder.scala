4921a78dc8c66cdc886e8a32d32a602bff639428
launvi <laura.nunez.villa@gmail.com>
2021-05-15 19:13:23 +0200

    Task1 solution

--- task1-isbn-encoder/src/main/scala/com/evobanco/test/IsbnEncoder.scala
+++ task1-isbn-encoder/src/main/scala/com/evobanco/test/IsbnEncoder.scala
@@ -1,4 +1,4 @@
-package com.evobanco.test.test
+package com.evobanco.test
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
@@ -15,6 +15,18 @@ class IsbnEncoderImplicit(df: DataFrame) extends Serializable {
     * @return a data frame with new rows for each element of the ISBN code
     */
   def explodeIsbn(): DataFrame = {
-    df
+
+    val udfGetIsbn = udf((originalISBN: String) => {
+
+      val isbnRegexPattern = "^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{3}-?[0-9]{10}$)(97[89])[-\\ ]?([0-9]{2})([0-9]{4})([0-9]{3})[0-9]{1}$".r
+      (originalISBN match {
+        case isbnRegexPattern(ean, group, publisher, title) => List(s"ISBN-EAN: $ean", s"ISBN-GROUP: $group", s"ISBN-PUBLISHER: $publisher",
+          s"ISBN-TITLE: $title")
+        case _ => List[String]()
+      }).:+(originalISBN)
+    }
+    )
+
+    df.withColumn("isbn", explode(udfGetIsbn(col("isbn"))))
   }
 }
