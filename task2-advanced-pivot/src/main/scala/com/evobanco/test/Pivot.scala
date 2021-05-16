e869364b3ac1f2020db7064693f3ec3895b6d499
launvi <laura.nunez.villa@gmail.com>
2021-05-16 10:16:23 +0200

    task2

--- task2-advanced-pivot/src/main/scala/com/evobanco/test/Pivot.scala
+++ task2-advanced-pivot/src/main/scala/com/evobanco/test/Pivot.scala
@@ -1,6 +1,7 @@
 package com.evobanco.test
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.{Window, WindowSpec}
 import org.apache.spark.sql.functions._
 
 object Pivot {
@@ -24,7 +25,31 @@ class PivotImplicit(df: DataFrame) extends Serializable {
     * @return machine data pivoted
     */
   def getTests(): DataFrame = {
+
+    val testDF = df.filter(col("mType") === Pivot.TypeTest).drop("mType","value")
+
+    val testWindow = Window.partitionBy("part","location","name")
+
+    val firstAndLastAggTests = getFirstAndLastAggregation(testDF, testWindow).withColumnRenamed("name","test")
+
+    val measurementWindow = Window.partitionBy("part","name")
+
+    val measurements = getFirstAndLastAggregation(df.filter(col("mType") =!= Pivot.TypeTest), measurementWindow)
+      .groupBy("part","aggregation").agg(collect_list(struct("name","value")).as("Features"))
+
+    firstAndLastAggTests.join(measurements, Seq("part", "aggregation"), "left")
+
+  }
+
+  def getFirstAndLastAggregation( df: DataFrame, window: WindowSpec) : DataFrame = {
     df
+      .withColumn("maxUnixTimeStamp", max(col("unixTimestamp")).over(window))
+      .withColumn("minUnixTimeStamp", min(col("unixTimestamp")).over(window))
+      .filter(col("unixTimestamp") === col("minUnixTimeStamp") || col("unixTimestamp") === col("maxUnixTimeStamp"))
+      .withColumn("aggregation", explode(
+        when(col("maxUnixTimeStamp") === col("minUnixTimeStamp"), array(lit("first"), lit("last")))
+          .otherwise(when(col("maxUnixTimeStamp") === col("unixTimestamp"), array(lit("last"))).otherwise(array(lit("first"))))))
+    .drop("minUnixTimeStamp", "maxUnixTimeStamp", "unixTimestamp")
   }
 
 }
